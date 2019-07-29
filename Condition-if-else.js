b992f38e2b7f28b91dbbeae6a947cca4f5f4eb4e
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-07-30 01:13:08 +0700

    JavaScript latihan Condition if else

---
+++ Condition-if-else.js
@@ -0,0 +1,16 @@
+var nama = 'gusti';
+var peran = 'Penyihir';
+if (nama === '' && peran === '') {
+    console.log('Nama harus diisi!');
+} else if (nama && peran === '') {
+    console.log('Halo ' + nama +', Pilih peranmu untuk memulai game!');
+} else if (nama && peran === 'Ksatria') {
+    console.log('Selamat datang di dunia proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+} else if (nama && peran === 'Tabib') {
+    console.log('Selamat datang di dunia proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.');
+}else if (nama && peran === 'Penyihir') {
+    console.log('Selamat datang di dunia proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+}
