cd9f59763f4a78b6a3f653cd99009b7464745819
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-07-30 17:07:59 +0700

    nomor 1 dari Looping

---
+++ Looping.js
@@ -0,0 +1,15 @@
+// 1. Melakukan Looping Menggunakan While
+console.log("LOOPING PERTAMA");
+var pertama = 0;
+
+while (pertama < 19) {
+  pertama = pertama + 2;
+  console.log(pertama + " - I love coding");
+}
+console.log("LOOPING KEDUA");
+var kedua = 22;
+
+while (kedua > 2) {
+  kedua = kedua - 2;
+  console.log(kedua + " - I will become fullstack developer");
+}
